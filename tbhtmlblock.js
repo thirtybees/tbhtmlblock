f4f377a72190739b061fcdbe2a6078d9a88610df
Markus Hitter <mah@jump-ing.de>
2019-07-05 18:13:06 +0200

    tbhtmlblock.js: formatting.
    No functional change.

--- tbhtmlblock.js
+++ tbhtmlblock.js
@@ -17,24 +17,25 @@
  */
 
 $(document).ready(function() {
-	$('table.tableDnD').tableDnD({
+  $('table.tableDnD').tableDnD({
+    onDragStart: function(table, row) {
+      originalOrder = $.tableDnD.serialize();
+      reOrder = ':even';
 
-		onDragStart: function(table, row) {
+      if (table.tBodies[0].rows[1]
+          && $('#' + table.tBodies[0].rows[1].id).hasClass('alt_row')
+      ) {
+        reOrder = ':odd';
+      }
 
-			originalOrder = $.tableDnD.serialize();
-			reOrder = ':even';
-
-			if (table.tBodies[0].rows[1] && $('#' + table.tBodies[0].rows[1].id).hasClass('alt_row'))
-				reOrder = ':odd';
-
-			$('#'+table.id+ '#' + row.id).parent('tr').addClass('myDragClass');
-			},
-		dragHandle: 'dragHandle',
-		onDragClass: 'myDragClass',
-		onDrop: function(table, row) {
-			var tableDrag = $('#' + table.id);
-			tableDrag.find('tr').not('.nodrag').removeClass('alt_row');
-			tableDrag.find('tr:not(".nodrag"):odd').addClass('alt_row');
-		}
-	});
+      $('#'+table.id+ '#' + row.id).parent('tr').addClass('myDragClass');
+    },
+    dragHandle: 'dragHandle',
+    onDragClass: 'myDragClass',
+    onDrop: function(table, row) {
+      var tableDrag = $('#' + table.id);
+      tableDrag.find('tr').not('.nodrag').removeClass('alt_row');
+      tableDrag.find('tr:not(".nodrag"):odd').addClass('alt_row');
+    }
+  });
 })
